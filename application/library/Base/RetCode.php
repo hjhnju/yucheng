efbb3ec9061ad677ef5d65407fcdefaeb1d486c7
hjhnju <hjh.nju@gmail.com>
2014-12-16 20:32:15 +0800

    add retcode

--- application/library/Base/RetCode.php
+++ application/library/Base/RetCode.php
@@ -1,7 +1,8 @@
 <?php
 /**
  * 异常码与描述定义类
- * 各模块可以在library中自定义错误码，如User/RetCode.php
+ * 通用状态码由Base_RetCode中定义，范围[0~1024], 各模块禁止使用1024内错误码
+ * 各模块业务状态码自定义，范围(1024, 65535], 如：library/User/RetCode.php, 继承library/Base/RetCode.php
  */
 
 class Base_RetCode {
@@ -12,18 +13,21 @@ class Base_RetCode {
      //默认错误
     const UNKNOWN_ERROR     = 99; //未知错误
 
+    //重定向
+    const REDIRECT          = 302; //前端重定向
+
     //验证会话
-    const SESSION_INVALID   = 410; //会话无效
-    const SESSION_DENY      = 411; //非法请求
-    const SESSION_NOT_LOGIN = 412; //未登录
+    const SESSION_INVALID   = 601; //会话无效
+    const SESSION_DENY      = 602; //非法请求
+    const SESSION_NOT_LOGIN = 603; //未登录
 
     //其他错误
-    const DB_ERROR          = 501; //数据库操作错误
-    const PARAM_ERROR       = 502; //接口参数错误
-    const NOT_FINISHED      = 503; //功能未实现
-    const DATA_NULL         = 504; //数据为空
-    const SERVICE_DEGRADED  = 505; //服务降级
-    const CONFIG_FAIL       = 506; //配置错误
+    const DB_ERROR          = 701; //数据库操作错误
+    const PARAM_ERROR       = 702; //接口参数错误
+    const NOT_FINISHED      = 703; //功能未实现
+    const DATA_NULL         = 704; //数据为空
+    const SERVICE_DEGRADED  = 705; //服务降级
+    const CONFIG_FAIL       = 706; //配置错误
 
     /* 消息函数
      * @var array
@@ -31,6 +35,7 @@ class Base_RetCode {
     protected static $_arrErrMap = array(
         self::SUCCESS           => '成功',
         self::UNKNOWN_ERROR     => '未知错误',
+        self::REDIRECT          => '前端重定向',
 
         self::SESSION_INVALID   => '会话无效',
         self::SESSION_DENY      => '非法请求',
