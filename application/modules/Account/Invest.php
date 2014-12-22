693b7655591fd83e7ca574962d5718dedaac37cc
jiangbianliming <jiangbianliming@126.com>
2014-12-22 18:13:37 +0800

    update interfaces

--- application/modules/Account/Invest.php
+++ application/modules/Account/Invest.php
@@ -12,8 +12,7 @@ class InvestController extends Base_Controller_Response {
 	
 	/**
 	 * 接口 /account/invest/index
-	 * 调用投资模块Invest_Api获取投资项目列表数据
-	 * @param status 投资项目状态   1--回款中(默认)  2--投标中 3--已结束  4--投标失败
+	 * 调用投资模块Invest_Api获取投资项目列表数据--拉取回款中
 	 * @param page begin from 1
 	 * @return 标准json
 	 * status 0 处理成功
@@ -21,9 +20,11 @@ class InvestController extends Base_Controller_Response {
 	 * data=
 	 * { 
 	 *     { 
+	 *         'proId' 投资项目id
 	 *         'investPro' 投资项目名称
-	 *         'annlnterestRate' 年化利率
+	 *         'annlnterestRate' 年利率
 	 *         'tenderAmt' 投标金额
+	 *         'deadline' 期限
 	 *         'tenderTime'投标时间
 	 *         'haveBack' 已回款
 	 *         'toBeBack' 待回款
@@ -37,6 +38,79 @@ class InvestController extends Base_Controller_Response {
 	}
 	
 	/**
+	 * 接口 /account/invest/tendering
+	 * 调用投资模块Invest_Api获取投资项目列表数据--拉取投标中
+	 * @param page begin from 1
+	 * status 0 处理成功
+	 * status 1108 获取投资列表失败
+	 * data=
+	 * { 
+	 *     { 
+	 *         'proId' 投资项目id
+	 *         'investPro' 投资项目名称
+	 *         'annlnterestRate' 年利率
+	 *         'tenderAmt' 投标金额
+	 *         'deadline' 期限
+	 *         'tenderTime'投标时间
+	 *         'tenderProgress' 进度
+	 *     }
+	 *  }
+	 */
+	public function  tenderingAction() {
+		
+	}
+	
+	/**
+	 * 接口/account/invest/ended 
+	 * 调用投资模块Invest_Api获取投资项目列表数据--拉取已结束
+	 * @param page begin from 1
+	 * status 0 处理成功
+	 * status 1108 获取投资列表失败
+	 * data=
+	 * { 
+	 *     { 
+	 *         'proId' 投资项目id
+	 *         'investPro' 投资项目名称
+	 *         'annlnterestRate' 年利率
+	 *         'tenderAmt' 投标金额
+	 *         'deadline' 期限
+	 *         'tenderTime'投标时间
+	 *         'endTime' 合同结束时间
+	 *         'totalRetAmt' 总回款
+	 *         'totalProfit'总收益
+	 *     }
+	 *  }
+	 */
+	public function  endedAction() {
+	
+	}
+	
+	/**
+	 * 接口/account/invest/tenderFail
+	 * 调用投资模块Invest_Api获取投资项目列表数据--拉取投标失败
+	 * @param page begin from 1
+	 * status 0 处理成功
+	 * status 1108 获取投资列表失败
+	 * data=
+	 * { 
+	 *     { 
+	 *         'proId' 投资项目id
+	 *         'investPro' 投资项目名称
+	 *         'annlnterestRate' 年利率
+	 *         'tenderAmt' 投标金额
+	 *         'deadline' 期限
+	 *         'tenderTime'投标时间
+     *         'failReason' 失败原因
+	 *     }
+	 *  }
+	 * 
+	 */
+	public function tenderFailAction() {
+		
+	}
+	
+	/**
+	 * 接口 /account/invest/repayPlan
 	 * 调用投资模块Invest_Api获取某个项目的还款计划
 	 * @param proId 项目ID
 	 * @return 标准json
@@ -56,7 +130,7 @@ class InvestController extends Base_Controller_Response {
 	 *     }
 	 *  }
 	 */
-	public function repayPlan() {
+	public function repayPlanAction() {
 		
 	}
 	
