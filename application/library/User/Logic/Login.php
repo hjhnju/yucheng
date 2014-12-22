b23aa87bb4e28f579d891cde14cefc3614b3704c
huwei <whu404@163.com>
2014-12-22 10:44:20 +0800

    code style fix

--- application/library/User/Logic/Login.php
+++ application/library/User/Logic/Login.php
@@ -6,6 +6,23 @@
 class User_Logic_Login{
 
     const SESSION_LOGIN_USER = "login_user";
+    
+    //第三方登录需要的配置信息
+    protected $third_login_array = array(
+    	'auth_code_redirect_url'         => 'http://www.xingjiaodai.cn:8301/User/Loginapi/thirdLogin',
+        'access_token_redirect_url'      => 'http://www.xingjiaodai.cn:8301/User/Loginapi/thirdLogin',
+        'qq'                             => array(
+    	    'host'           => 'https://graph.qq.com',
+            'appid'          => '101177204',
+            'appkey'         => 'd3aed93ef6e8e009ca30dcd33eb12093',
+            'authcode_url'   => '/oauth2.0/authorize?response_type=code&client_id=',
+            'acctoken_url'   => '/oauth2.0/token?grant_type=authorization_code&client_id=',
+            'openid_url'     => '/oauth2.0/me?access_token=',
+            'getinfo_url'    => '/user/get_user_info?format=json&oauth_consumer_key=d3aed93ef6e8e009ca30dcd33eb12093&access_token=',
+        ),
+        'weibo'                          => array(),
+        'weixin'                         => array(),
+    );
 
     public function __construct(){
     }
@@ -40,9 +57,8 @@ class User_Logic_Login{
      */
     public function getAuthCodeUrl($strType){
         Yaf_Session::getInstance()->set("third_login_type",$strType);
-        $arrData =  Base_Config::getConfig('login');
-        $redirect_url = $arrData['auth_code_url'];
-        $arrData = $arrData[$strType];
+        $redirect_url = $this->third_login_array['auth_code_redirect_url'];
+        $arrData = $this->third_login_array[$strType];
         $host = $arrData['host'];
         $randnum = md5(uniqid(rand(), TRUE));
         Yaf_Session::getInstance()->set("state",$randnum);
@@ -57,10 +73,9 @@ class User_Logic_Login{
      * 获取access token
      */
     public function getAccessToken($strAuthCode){
-        $strType = Yaf_Session::getInstance()->get("third_login_type");
-        $arrData =  Base_Config::getConfig('login');
-        $redirect_url = $arrData['access_token_url'];
-        $arrData = $arrData[$strType];
+        $strType = Yaf_Session::getInstance()->get("third_login_type");        
+        $redirect_url = $this->third_login_array['access_token_redirect_url'];
+        $arrData = $this->third_login_array[$strType];
         $host = $arrData['host'];
         $url = $arrData['acctoken_url'].$arrData['appid']."&client_secret=".$arrData['appkey']."&code=$strAuthCode"."&redirect_uri=".$redirect_url;
         $post = Base_Network_Http::instance()->url($host,$url);
@@ -84,9 +99,8 @@ class User_Logic_Login{
      */
     public function getOpenid($access_token){
         $strType = Yaf_Session::getInstance()->get("third_login_type");
-        $arrData =  Base_Config::getConfig('login');
-        $redirect_url = $arrData['access_token_url'];
-        $arrData = $arrData[$strType];
+        $redirect_url = $this->third_login_array['access_token_url'];
+        $arrData = $this->third_login_array[$strType];
         $host = $arrData['host'];
         $redirect_url = $arrData['openid_url'].$access_token;
         $post = Base_Network_Http::instance()->url($host,$redirect_url);
@@ -102,38 +116,41 @@ class User_Logic_Login{
     }
     
     /**
-     * 获取第三方站点信息,目前只返回nickname
+     * 获取第三方站点信息, 如果为空返回NULL,否则返回user的json对象
+     * 失败返回空串
      */
     public function getUserThirdInfo($openid){
         $strType = Yaf_Session::getInstance()->get("third_login_type");
+        if(!isset($_COOKIE['access_key'])){
+           return NULL; 
+        }
         $key = $_COOKIE['access_key'];
         $access_token = Base_Redis::getInstance()->get("access_token".$strType.$key);
-        $strType = Yaf_Session::getInstance()->get("third_login_type");
-        $arrData =  Base_Config::getConfig('login');
-        $arrData = $arrData[$strType];
+        $arrData = $this->third_login_array[$strType];
         $host = $arrData['host'];
         $redirect_url = $arrData['getinfo_url'].$access_token.'openid='.$openid;
         $post = Base_Network_Http::instance()->url($host,$redirect_url);
         $response = $post->exec();
         $user = json_decode($response);
         if (!isset($user->nickname)){
-            return $this->ajaxError();
+            return NULL;
         }
-        return $user->nickname;
+        return $user;
     }
     
     /**
      * 检查第三方的绑定状态
      * @param unknown $openid
      * @param unknown $intType
+     * 绑定返回true,没绑定返回false
      */
     public function checkBind($openid,$strType){
         $objThird = new User_Object_Third();
         $objThird->fetch(array('openid'=>$openid,'authtype'=>$this->getAuthType($strType)));
         if(empty($objThird->userid)){
-            return User_RetCode::UNBOUND;
+            return false;
         }   
-        return User_RetCode::BOUND;
+        return true;
     }
     
     /**
@@ -142,12 +159,13 @@ class User_Logic_Login{
      * @param unknown $type
      * @param unknown $strName
      * @param unknown $strPasswd
+     * 成功返回true,失败返回false
      */
     public function setBind($openid, $type,$strName,$strPasswd){
         $objLogin         = new User_Object_Login();
         $objLogin->fetch(array($this->checkType($strName)=>$strName,'passwd'=>md5($strPasswd)));
         if(empty($objLogin->userid)){
-            return User_RetCode::UNBOUND;
+            return false;
         }
         $this->setLogin($objLogin);
         $objThird     = new User_Object_Third();
@@ -157,9 +175,9 @@ class User_Logic_Login{
         $objThird->openid = $openid;
         $ret = $objThird->save();
         if($ret){
-            return User_RetCode::BOUND;
+            return true;
         }
-        return User_RetCode::UNBOUND;
+        return false;
     }
 
     /**
@@ -217,7 +235,10 @@ class User_Logic_Login{
         $arr = array(
             'qq'    => 1,
             'weibo' => 2,);
-        return $arr[$strType];
+        if(isset($arr[$strType])){
+            return $arr[$strType];
+        }
+        return '';
     }
   
 }
