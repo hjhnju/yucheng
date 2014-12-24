838fb11175c9bc7d8ed4fbe34156a0204690c229
mySunShinning <215234183@qq.com>
2014-12-24 15:43:40 +0800

    smarty and my invest

--- fe/src/my/invest/index.js
+++ fe/src/my/invest/index.js
@@ -9,58 +9,115 @@
 define(function (require) {
     var $ = require('jquery');
     var etpl = require('etpl');
+    var moment = require('moment');
     var commonDate = require('common/data');
     var header = require('common/header');
     var Pager = require('common/ui/Pager/Pager');
     var Remoter = require('common/Remoter');
-    var getInvestList = new Remoter('MY_INVEST_GET');
-    var getInvestDetail = new Remoter('MY_INVEST_DETAIL');
+    var getReturnList = new Remoter('MY_INVEST_GET');
+    var getReturnDetail = new Remoter('MY_INVEST_DETAIL');
+    var getTenderingList = new Remoter('MY_INVEST_TENDERING');
 
     var tpl = require('./list.tpl');
 
+    // 分页对象
     var pager;
+
+    // 记录列表状态
     var status = 1;
+
+    // 用来记录被点击的回款计划按钮
     var item = null;
 
+    // 时间格式化
+    var FORMATER = 'YYYY-MM-DD hh:mm';
+
+    var htmlContainer;
+
+    /**
+     * 初始化方法
+     *
+     * @public
+     */
     function init() {
+        htmlContainer = $('#my-invest-list');
         header.init();
         etpl.compile(tpl);
 
-        getInvestList.remote({
-            page: 1,
-            status: status
+        getReturnList.remote({
+            page: 1
         });
 
         ajaxCallBack();
         bindEvents();
+
     }
 
+    /**
+     * 绑定dom事件
+     *
+     * @inner
+     */
     function bindEvents() {
+        // 选择投资类型
+        $('.my-invest-tab-item').click(function () {
+            if (!$(this).hasClass('current')) {
+                // 改变选中状态
+                $('.my-invest-tab-item').removeClass('current');
+                $(this).addClass('current');
+
+                // 记录当前选中类型
+                status = +$.trim($(this).attr('data-value'));
+
+                // 获取数据
+                getRemoteList(1);
+            }
+        });
+
+        // 获取回款列表按钮
         $('.my-invest-list').delegate('.view-plan-btn', 'click', function () {
             var value = $.trim($(this).attr('data-id'));
             item = $(this);
 
             $('.my-invest-item').removeClass('current');
             $(this).closest('.my-invest-item').addClass('current');
-            getInvestDetail.remote({
+            getReturnDetail.remote({
                 id: value
             });
         });
     }
 
+    /**
+     * 绑定请求回调
+     *
+     * @inner
+     */
     function ajaxCallBack() {
-        getInvestList.on('success', function (data) {
+        // 回款中列表
+        getReturnList.on('success', function (data) {
             if (data.bizError) {
                  alert(data.statusInfo);
             }
             else {
-                $('#my-invest-list').html(etpl.render('returnMoneyList', {
-                    list: data.list
-                }));
+                if (!pager) {
+                    pager = new Pager($.extend({}, commonDate.pagerOpt, {
+                        main: $('#my-invest-pager'),
+                        total: +data.pageall
+                    }));
+                    
+                    pager.render(+data.page);
+                    
+                    pager.on('change', function(e) {
+                        getRemoteList(e.value);
+                    });
+                }
+
+                renderHTML('returnMoneyList', data);
             }
         });
-
-        getInvestDetail.on('success', function (data) {
+        
+        // 还款计划
+        getReturnDetail.on('success', function (data) {
             if (data.bizError) {
                 alert(data.statusInfo);
             }
@@ -71,11 +128,55 @@ define(function (require) {
                 var container = $(item).closest('.my-invest-item')
                     .addClass('current').find('.my-invest-detail');
 
+                for (var i = 0, l = data.list.length; i < l; i++) {
+                    data.list[i].timeInfo = moment.unix(data.list[i].time).format('YYYY-MM-DD');
+                }
+
                 container.html(etpl.render('returnMoneyDetail', {
                     data: data
                 }));
             }
         });
+
+        // 投标中列表
+        getTenderingList.on('success', function (data) {
+            if (data.bizError) {
+                alert(data.statusInfo);
+            }
+            else {
+                renderHTML('tenderingList', data);
+            }
+        });
+    }
+    
+    function getRemoteList(page) {
+        switch (status) {
+            case 1:
+                getReturnList.remote({
+                    page: page
+                });
+                break;
+            case 2:
+                getTenderingList.remote({
+                    page: page
+                });
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+    }
+
+    function renderHTML(tpl, data) {
+        // 格式化时间
+        for (var i = 0, l = data.list.length; i < l; i++) {
+            data.list[i].timeInfo = moment.unix(data.list[i].tenderTime).format(FORMATER);
+        }
+
+        htmlContainer.html(etpl.render(tpl, {
+            list: data.list
+        }));
     }
 
     return {
