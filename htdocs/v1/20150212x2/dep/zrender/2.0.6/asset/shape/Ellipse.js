5e24efc3b523f040527735f67c12c25aafa7931b
yangbinYB <1033371745@qq.com>
2015-02-12 08:15:35 +0800

    fix reward

---
+++ htdocs/v1/20150212x2/dep/zrender/2.0.6/asset/shape/Ellipse.js
@@ -0,0 +1,48 @@
+define('zrender/shape/Ellipse', [
+    'require',
+    './Base',
+    '../tool/util'
+], function (require) {
+    var Base = require('./Base');
+    var Ellipse = function (options) {
+        Base.call(this, options);
+    };
+    Ellipse.prototype = {
+        type: 'ellipse',
+        buildPath: function (ctx, style) {
+            var k = 0.5522848;
+            var x = style.x;
+            var y = style.y;
+            var a = style.a;
+            var b = style.b;
+            var ox = a * k;
+            var oy = b * k;
+            ctx.moveTo(x - a, y);
+            ctx.bezierCurveTo(x - a, y - oy, x - ox, y - b, x, y - b);
+            ctx.bezierCurveTo(x + ox, y - b, x + a, y - oy, x + a, y);
+            ctx.bezierCurveTo(x + a, y + oy, x + ox, y + b, x, y + b);
+            ctx.bezierCurveTo(x - ox, y + b, x - a, y + oy, x - a, y);
+            ctx.closePath();
+        },
+        getRect: function (style) {
+            if (style.__rect) {
+                return style.__rect;
+            }
+            var lineWidth;
+            if (style.brushType == 'stroke' || style.brushType == 'fill') {
+                lineWidth = style.lineWidth || 1;
+            } else {
+                lineWidth = 0;
+            }
+            style.__rect = {
+                x: Math.round(style.x - style.a - lineWidth / 2),
+                y: Math.round(style.y - style.b - lineWidth / 2),
+                width: style.a * 2 + lineWidth,
+                height: style.b * 2 + lineWidth
+            };
+            return style.__rect;
+        }
+    };
+    require('../tool/util').inherits(Ellipse, Base);
+    return Ellipse;
+});
---
+++ htdocs/v1/20150212x2/dep/zrender/2.0.6/asset/tool/log.js
@@ -0,0 +1,19 @@
+define('zrender/tool/log', [
+    'require',
+    '../config'
+], function (require) {
+    var config = require('../config');
+    return function () {
+        if (config.debugMode === 0) {
+            return;
+        } else if (config.debugMode == 1) {
+            for (var k in arguments) {
+                throw new Error(arguments[k]);
+            }
+        } else if (config.debugMode > 1) {
+            for (var k in arguments) {
+                console.log(arguments[k]);
+            }
+        }
+    };
+});
