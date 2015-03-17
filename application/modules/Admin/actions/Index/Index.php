18ce313639434c2197e21f22dac3306c27b46bae
jiangbianliming <jiangbianliming@126.com>
2015-03-17 14:36:11 +0800

    Admin dashBoard modify

--- application/modules/Admin/actions/Index/Index.php
+++ application/modules/Admin/actions/Index/Index.php
@@ -5,6 +5,32 @@
  *
  */
 class IndexAction extends Yaf_Action_Abstract {
-    public function execute() {
-    }
+	/**
+	 * TODO：Admin首页展示数据获取
+	 * @author yibing
+	 *
+	 */
+	public function execute() {
+    		
+    		$list = new User_List_Login();
+    		$list = $list->toArray();
+    	    $userCount  = $list['total'];
+            $this->getView()->assign('userCount', $userCount);
+            
+            $loan = new Loan_List_Loan();
+            $loan->setFilterString('status = 5');
+            $loanAmount = $loan->sumField('amount');
+            $loanAmount =  $loanAmount/10000;
+            $this->getView()->assign('loanAmount', $loanAmount);
+            $clientAmount = $loan->distinCount('user_id');
+            $this->getView()->assign('clientAmount', $clientAmount); 
+
+            $invest = new Invest_List_Invest();
+            $investAmount = $invest->distinCount('user_id');
+            $this->getView()->assign('investAmount', $investAmount);
+    	}
 }
+
+
+
+
