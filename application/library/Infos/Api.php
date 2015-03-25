00de3d67a53b4c89a85b97297f0079b0117e1ded
jiangbianliming <jiangbianliming@126.com>
2015-03-25 18:29:03 +0800

    添加获取新闻

--- application/library/Infos/Api.php
+++ application/library/Infos/Api.php
@@ -7,27 +7,14 @@ class Infos_Api {
     /** 
      * Infos_Api::getNewPost()
      * 获取最新公告
+     * param int $page,int $pageSize, str $strType
+     * 页数 页面大小 公告类型（media，newpost）
      * @return $arrPost | null
      */
-    public static function getNewPost($strType){
+    public static function getNewPost($page,$pageSize,$strType){
         $logic   = new Infos_Logic_Post();
-        $list    = $logic->getList(1, 1,$strType);
-        unset($list['list'][0]['content']);
-        $ret = isset($list['list'][0]) ? $list['list'][0] : null;
-        return $ret;
-    }
-    
-    /**
-     * Infos_Api::getNewMedia()
-     * 获取媒体新闻
-     * @return $arrPost | null
-     */
-    public static function getNewMedia($strType){
-    	$logic   = new Infos_Logic_Post();
-    	$list    = $logic->getList(2, 1,$strType);
-    	unset($list['list'][0]['content']);
-    	$ret = isset($list['list'][0]) ? $list['list'][0] : null;
-    	return $ret;
+        $list    = $logic->getList($page,$pageSize,$strType);
+        return $list['list'];
     }
         
     /**
