83a34de6510b66ba7902da30745b7c012ba6e989
jiangbianliming <jiangbianliming@126.com>
2015-03-26 18:14:02 +0800

    fixbug

--- application/modules/Admin/actions/Loan/Getdetail.php
+++ application/modules/Admin/actions/Loan/Getdetail.php
@@ -14,7 +14,7 @@ class GetdetailAction extends Base_Controller_Action {
         
         $detail = Loan_Api::getLoanDetail($loanId);
         //对信息进行编码
-        $detail['content'] = URLEncode($detail['content']);
+        $detail['content'] = urlencode($detail['content']);
         //将编码信息中的+号变成空格
         $detail['content'] = str_replace('+', '%20', $detail['content']);
         Base_Log::debug(array('detail'=>$detail));
