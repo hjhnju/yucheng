5d9ebe39a8bd059bdb0ae533a0a775b908aa9683
jiangbianliming <jiangbianliming@126.com>
2015-03-28 18:41:14 +0800

    文字截取

--- application/modules/Admin/actions/Info/Push.php
+++ application/modules/Admin/actions/Info/Push.php
@@ -18,10 +18,28 @@ class PushAction extends Yaf_Action_Abstract {
 					$arrInfos[$key]['type'] = '媒体报道';
 					break;
 				case 3:
-					$arrInfos[$key]['type'] = '还款公告';
+					$arrInfos[$key]['type'] = '还款公告'; 
 					break;	
 			}
+			$arrInfos[$key]['abstract'] = $this->cutstr($val['abstract'],80);
+			$arrInfos[$key]['title'] = $this->cutstr($val['title'],40);
         }
         $this->getView()->assign('arrInfo', $arrInfos);
+    }
+    
+    /*
+     * TODO：文字截取并加点
+    * param str $string 要截取的字符串 int $length 截取的长度
+    */
+    function cutstr($string, $length) {
+    	preg_match_all("/[\x01-\x7f]|[\xc2-\xdf][\x80-\xbf]|\xe0[\xa0-\xbf][\x80-\xbf]|[\xe1-\xef][\x80-\xbf][\x80-\xbf]|\xf0[\x90-\xbf][\x80-\xbf][\x80-\xbf]|[\xf1-\xf7][\x80-\xbf][\x80-\xbf][\x80-\xbf]/", $string, $info);
+    	for($i=0; $i<count($info[0]); $i++) {
+    		$wordscut .= $info[0][$i];
+    		$j = ord($info[0][$i]) > 127 ? $j + 2 : $j + 1;
+    		if ($j > $length - 3) {
+    			return $wordscut." ...";
+    		}
+    	}
+    	return join('', $info[0]);
     }
 }
