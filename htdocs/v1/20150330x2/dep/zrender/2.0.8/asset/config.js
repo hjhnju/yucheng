03f03571a699183daee2d21af928e0c739c16abc
jiangbianliming <jiangbianliming@126.com>
2015-03-30 18:32:38 +0800

    首页改版

---
+++ htdocs/v1/20150330x2/dep/zrender/2.0.8/asset/config.js
@@ -0,0 +1,27 @@
+define('zrender/config', [], function () {
+    var config = {
+            EVENT: {
+                RESIZE: 'resize',
+                CLICK: 'click',
+                DBLCLICK: 'dblclick',
+                MOUSEWHEEL: 'mousewheel',
+                MOUSEMOVE: 'mousemove',
+                MOUSEOVER: 'mouseover',
+                MOUSEOUT: 'mouseout',
+                MOUSEDOWN: 'mousedown',
+                MOUSEUP: 'mouseup',
+                GLOBALOUT: 'globalout',
+                DRAGSTART: 'dragstart',
+                DRAGEND: 'dragend',
+                DRAGENTER: 'dragenter',
+                DRAGOVER: 'dragover',
+                DRAGLEAVE: 'dragleave',
+                DROP: 'drop',
+                touchClickDelay: 300
+            },
+            catchBrushException: false,
+            debugMode: 0,
+            devicePixelRatio: Math.max(window.devicePixelRatio || 1, 1)
+        };
+    return config;
+});
