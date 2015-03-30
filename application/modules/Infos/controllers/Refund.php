95ef8fffeba7deee8e2835d9065bdf750d3f9708
jiangbianliming <jiangbianliming@126.com>
2015-03-30 13:04:19 +0800

    refundPost

---
+++ application/modules/Infos/controllers/Refund.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * 还款公告
+ */
+class RefundController extends Base_Controller_Page {
+
+    public function init(){
+        $this->setNeedLogin(false);
+        parent::init();
+    }
+    
+    /**
+     * 还款公告列表页
+     *
+     * /infos/refund
+     * @param   $page, MUST, [1,-), index of page
+     * @assign  data=>array('page', 'pagesize', 'pageall', 'list', 'total')
+     */
+    public function indexAction() {
+        $page     = isset($_REQUEST['page']) ? intval($_REQUEST['page']) : 0;
+        $pagesize = 10;
+        $logic    = new Infos_Logic_Post();
+        $ret      = $logic->getList($page, $pagesize,'refundPost');
+        Base_Log::notice($ret);
+        $this->getView()->assign('data', $ret);
+    }
+     
+ 
+
+    /**
+     * 还款公告详情页
+     *
+     * /infos/refund/detail?id=
+     * @param   $id 公告id
+     * @assign  data=>array('title','ctx','author','publish_time')
+     */
+    public function detailAction() {
+        $postid = isset($_GET['id'])? intval($_GET['id']) : 0; 
+        $logic  = new Infos_Logic_Post();
+        $ret    = $logic->getPost($postid);
+        Base_Log::notice(array(
+            'title' => isset($ret['title'])? $ret['title'] : ''));
+        $this->getView()->assign('data', $ret);
+    }
+}
