f4ea0c5162225efd119fd7e539f3b7019e7a6ef1
jiangbianliming <jiangbianliming@126.com>
2015-03-30 21:15:17 +0800

    修改数据未定义

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -73,6 +73,8 @@ class IndexController extends Base_Controller_Page {
      */
     function cutstr($string, $length) {
     	preg_match_all("/[\x01-\x7f]|[\xc2-\xdf][\x80-\xbf]|\xe0[\xa0-\xbf][\x80-\xbf]|[\xe1-\xef][\x80-\xbf][\x80-\xbf]|\xf0[\x90-\xbf][\x80-\xbf][\x80-\xbf]|[\xf1-\xf7][\x80-\xbf][\x80-\xbf][\x80-\xbf]/", $string, $info);
+    	$j = 0;
+    	$wordscut = '';
     	for($i=0; $i<count($info[0]); $i++) {
     		$wordscut .= $info[0][$i]; 
     		$j = ord($info[0][$i]) > 127 ? $j + 2 : $j + 1;
