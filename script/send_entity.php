9775e6edf7c701875babca0ec880e9aff858712f
hjhfdu <hjh_nju@sohu.com>
2015-04-17 17:35:29 +0800

    添加实物奖励入库

--- script/send_entity.php
+++ script/send_entity.php
@@ -1,7 +1,8 @@
 <?php
-require(dirname(__FILE__) . '/../env.inc.php');
+require(dirname(__FILE__) . '/env.inc.php');
 
 //读20150415_award.txt表,按手机号获取用户
 $activity = new Awards_Activity_Invite20150415();
-$bol = $activity->giveAward($userid);
+$arrParam = array();
+$bol = $activity->giveAward($userid, $arrParam);
 
