422216b41bc38255d80f956620abb4b9b0ab8d40
jiangbianliming <jiangbianliming@126.com>
2015-04-30 09:34:17 +0800

    增加微信登录

--- application/modules/M/controllers/Login.php
+++ application/modules/M/controllers/Login.php
@@ -14,20 +14,18 @@ class LoginController extends Base_Controller_Page {
     }
     /**
      * 登录
-     *
+     * 微信授权链接为：https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx8caa620ee9e4503a&redirect_uri=http://www.xingjiaodai.com/m/login/weixin&response_type=code&scope=snsapi_login#wechat_redirect
      * /m/login
      * @param   
      * @assign   
      */
     public function indexAction() {
-    	$this->getView()->assign('title', "登陆兴教贷");
-
+    	$this->getView()->assign('title', "登录兴教贷");
         $logic  = new User_Logic_Login();
         $userid = $logic->checkLogin();
         if($userid){
             $this->redirect('/account/overview');
         }
-
         $u = isset($_REQUEST['u'])?trim($_REQUEST['u']):null;
         if(!empty($u)){
             $strRedirect = $u;
@@ -41,6 +39,50 @@ class LoginController extends Base_Controller_Page {
         }
     }
     
- 
-
+    /**
+     * 微信登录或绑定账号
+     */
+    public function WeixinAction(){
+        $strType     = 'weixin';
+        $strAuthCode = isset($_REQUEST['code'])?$_REQUEST['code']:'';
+        $logicThird       = new User_Logic_Third();
+        if(!empty($strAuthCode)){           
+            $arrRet  = $logicThird->WeixinLogin($strAuthCode);      
+            $openid  = $arrRet['openid'];
+            $token   = $arrRet['token'];
+        }
+        if(empty($openid)){
+            return $this->ajaxError(User_RetCode::GET_OPENID_FAIL,
+                    User_RetCode::getMsg(User_RetCode::GET_OPENID_FAIL));
+        }
+        Yaf_Session::getInstance()->set(User_Keys::getAuthTypeKey(),$strType);
+        Yaf_Session::getInstance()->set(User_Keys::getOpenidKey(), $openid);
+        Base_Redis::getInstance()->set(User_Keys::getAccessTokenKey($strType, $openid), $token, 30*24*3600);
+        //是否已有绑定账号
+        $userid = $logicThird->getBindUserid($openid, $strType);
+        
+        if(!empty($userid)){
+            //已绑定的用户登录成功
+            $objUser = new User_Object($userid);
+            $logic   = new User_Logic_Login();
+            $logic->setLogin($objUser);
+            Base_Log::notice(array(
+            'msg'  => 'success',
+            'type' => $strType,
+            'code' => $strAuthCode,
+            'openid' => $openid,
+            ));
+            $this->redirect('/account/overview');
+        }else{
+            //用户未绑定账号
+            //session 中已存openid
+            Base_Log::notice(array(
+            'msg' => 'redirect to regist bind',
+            'type' => $strType,
+            'code' => $strAuthCode,
+            'openid' => $openid,
+            ));            
+            $this->redirect('/m/regist?type=weixin');
+        }       
+    }
 }
