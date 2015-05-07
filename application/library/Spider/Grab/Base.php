8e4cf4c26a55618edc2a2071e16d37ef7679178f
guojinli <jiangbianliming@126.com>
2015-05-07 18:24:44 +0800

    add basic spider structure

---
+++ application/library/Spider/Grab/Base.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Class Spider_Grab_base
+ * 抓取网页基本类
+ */
+class Spider_Grab_Base{
+    public function __construct(){
+
+    }
+}
---
+++ application/modules/Admin/controllers/Collect.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * 提取数据的控制器
+ */
+class CollectController extends Base_Controller_Admin{
+}
