3ef0e68a2783926820bb543b6328aa5bf0f6066c
jiangbianliming <jiangbianliming@126.com>
2015-05-28 12:16:26 +0800

    天使列表及收益

---
+++ application/modules/Account/controllers/Angelprofit.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ */
+class AngelprofitController extends Base_Controller_Page {
+    
+    CONST PAGESIZE = 10;    
+
+    public function init() {
+        parent::init();
+    }
+    
+    /**
+     * assign至前端邀请url
+     * inviteUrl 用户的专属邀请链接
+     * userinfo 左上角信息
+     */
+    public function indexAction() {             
+        $userid     = $this->userid;    
+        $webroot    = Base_Config::getConfig('web')->root;
+        
+        $logic     = new Account_Logic_UserInfo();
+        $userInfo  = $logic->getUserInfo($this->objUser);
+        
+        $this->getView()->assign('userinfo', $userInfo);
+    }
+  
+}
