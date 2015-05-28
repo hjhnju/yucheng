23e43023529fd28a417d89823016df3c9c04bbc8
jiangbianliming <jiangbianliming@126.com>
2015-05-28 12:59:06 +0800

    客发后台升级

--- application/modules/Admin/actions/Kefa/Detail.php
+++ application/modules/Admin/actions/Kefa/Detail.php
@@ -16,7 +16,7 @@ class DetailAction extends Yaf_Action_Abstract
         $info['id'] = $id;
       
         $this->strFilter($info);
-        if((' '==$info['tag']) ||(empty($info['tag']))){
+        if((''==$info['tag']) ||(empty($info['tag']))){
             $info['tag'] = '待跟进';
         }
         $this->getView()->assign('info', $info);
@@ -24,19 +24,21 @@ class DetailAction extends Yaf_Action_Abstract
         
         if ('save' == $type) {
             $arrRet = array(
-               'name'       => $_REQUEST['name'],
-               'address'    => $_REQUEST['address'],
-               'president'  => $_REQUEST['president'],
-               'phone'      => $_REQUEST['phone'],
-               'weight'     => $_REQUEST['weight'],
-               'website'    => $_REQUEST['website'],
-               'introduct'  => $_REQUEST['introduct'],
-               'regulation' => $_REQUEST['regulation'],
-               'evaluate' => $_REQUEST['evaluate'],
-               'tag' => $_REQUEST['tag'],
+               'name'       => isset($_REQUEST['name'])?$_REQUEST['name']:'',
+               'address'    => isset($_REQUEST['address'])?$_REQUEST['address']:'',
+               'president'  => isset($_REQUEST['president'])?$_REQUEST['president']:'',
+               'phone'      => isset($_REQUEST['phone'])?$_REQUEST['phone']:'',
+               'weight'     => isset($_REQUEST['weight'])?$_REQUEST['weight']:'',
+               'website'    => isset($_REQUEST['website'])?$_REQUEST['website']:'',
+               'introduct'  => isset($_REQUEST['introduct'])?$_REQUEST['introduct']:'',
+               'regulation' => isset($_REQUEST['regulation'])?$_REQUEST['regulation']:'',
+               'evaluate'   => isset($_REQUEST['evaluate'])?$_REQUEST['evaluate']:'',
+               'tag'        => isset($_REQUEST['tag'])?$_REQUEST['tag']:'',
             );
             foreach ($arrRet as $key => $val){
-                $redis->hset("hset_school_".$id, $key , $val);
+                if(!empty($val)){
+                    $redis->hset("hset_school_".$id, $key , $val);
+                }
             }
         }
     }
@@ -48,13 +50,22 @@ class DetailAction extends Yaf_Action_Abstract
         $arrFilters = array('<br />','<p>','</p>');
         foreach ($arrFields as $field){
             if(!isset($info[$field])){
-                $info[$field] = ' ';
+                $info[$field] = '';
             }
         }
         foreach ($arrFilters as $filter){
             foreach ($info as $key => $val){
-                $info[$key] = str_ireplace($filter,"",$val);
+               $info[$key] = str_ireplace($filter,"",$val);
             }
         }
+        
+        /*foreach ($info as $key => $val){
+            foreach ($arrFilters as $filter){
+                $info[$key] = str_ireplace($filter,"",$val);
+            }
+            if(strstr($val,"暂无")){
+                $info[$key] = '';
+            }
+        }*/
     }
 }
--- script/export/export_invite.php
+++ script/export/export_invite.php
@@ -1,32 +1,108 @@
 <?php
-require(dirname(__FILE__) . '/env.inc.php');
-$id = 100;
-
-$sql = "SELECT userid, count(invitee) as cnt 
-	FROM `xjd`.`user_invite` where create_time>=1429027200 group by userid order by cnt desc limit 0,600";
-
+require(dirname(__DIR__) . '/env.inc.php');
+$startTime = strtotime("2015-04-15 00:00:00");
+$endTime   = strtotime("2015-05-15 23:59:59");
+$sql = "SELECT userid, count(invitee) as cnt FROM `xjd`.`user_invite` where create_time>=".$startTime." and create_time<=".$endTime." group by userid order by cnt desc limit 0,200";
 $arrRow = Base_Db::getInstance('xjd')->fetchAll($sql);
+$arrResult = array();
+$fp = fopen("fakeresult.txt","w");
+$fpr = fopen("realresult.txt","w");
+$fp1 = fopen("fakepaiming.txt","w");
+$fp2 = fopen("realpaiming.txt","w");
 foreach ($arrRow as $row) {
-	if($row['cnt'] < 6){
+	/*if($row['cnt'] < 6){
 		continue;
-	}
+	}*/
 	$userid = $row['userid'];
-	$sql = "SELECT invitee, create_time FROM `xjd`.`user_invite` where userid=".$userid." order by create_time asc";
+	$sql = "SELECT invitee, create_time FROM `xjd`.`user_invite` where userid=".$userid." and create_time<=".$endTime." order by create_time asc";
 
 	$arrRow2 = Base_Db::getInstance('xjd')->fetchAll($sql);
 	$cnt = 0;
+	$arrIp = array();
+	$arrHufu = array();
+	$arrInvite = array();
 	foreach ($arrRow2 as $row2) {
-		$cnt = $cnt + 1;
-		if($cnt === 6){
-			$lastTime = $row2['create_time'];
+		$objUser = User_Api::getUserObject($row2['invitee']);
+		$info = User_Api::getUserObject($userid);
+		
+		$huifu = $objUser->huifuid;
+		if(!empty($huifu)){
+		    $arrIp[] = $objUser->lastip;
+		    $arrHufu[] = $objUser->huifuid;
 		}
-		$arrOut = array($userid, $row2['invitee'], strftime('%Y-%m-%d %H:%M:%S', $row2['create_time']), 
-			strftime('%Y-%m-%d %H:%M:%S', $lastTime));
+		
+		$arrInvite[] = $row2['invitee'];
+		$name = $info->name;
+		if(empty($name)){
+		    $info->name = $info->phone;
+		    $infostarname = Base_Util_String::starPhone($info->phone);
+		}else{
+		    $info->name = $name;
+		    $infostarname = Base_Util_String::starUsername($name);
+		}
 
-		$objUser = User_Api::getUserObject($userid);
-		echo implode(',', $arrOut);
+		$name = $objUser->name;
+		if(empty($name)){
+		    $objUser->name = $objUser->phone;
+		    $objUserstarname = Base_Util_String::starPhone($objUser->phone);
+		}else{
+		    $objUser->name = $name;
+		    $objUserstarname = Base_Util_String::starUsername($name);
+		}
+		$arrOut = array($userid, $info->name,$infostarname, $row2['invitee'],$objUser->name, $objUserstarname, strftime('%Y-%m-%d %H:%M:%S', $row2['create_time']), 
+			$objUser->lastip,$objUser->huifuid);
+		fwrite($fp,implode(',', $arrOut)."\n");
 	}
-}
-
+	
+	
+	$arrTemp['id'] = $userid; 
+	$arrTemp['name'] = $info->name;
+	if(empty($arrTemp['name'])){
+	    $arrTemp['name'] = $info->phone;
+	    $arrTemp['starname'] = Base_Util_String::starPhone($info->phone);
+	}else{
+	    $arrTemp['starname'] = Base_Util_String::starUsername($info->name);
+	}
+	$arrTemp['fake'] = count($arrInvite);
+	$arrTemp['invite'] = $arrInvite;
+	
+	$arrIp = array_unique($arrIp);
+	$arrHufu = array_unique($arrHufu);
+	$arrTemp['huifu'] = count($arrHufu);
+	fwrite($fp1,$userid.",".$arrTemp['name'].",".$arrTemp['starname'].",".count($arrInvite).",".count($arrIp).",".count($arrHufu)."\n");
 
 	
+	$arrTemp['real'] = count($arrIp);
+	$arrResult[] = $arrTemp;
+}
+
+$counts = array();
+foreach ($arrResult as $user) {
+    $counts[] = $user['real'];
+}
+array_multisort($counts, SORT_DESC , $arrResult);
+foreach ($arrResult as $val){
+    $arrIpTemp = array();
+    foreach ($val['invite'] as $data){
+        $objUser = User_Api::getUserObject($data);
+        $invite = new User_Object_Invite();
+        $invite->fetch(array('userid'=>$val['id'],'invitee'=>$data));
+        if(!in_array($objUser->lastip,$arrIpTemp)){
+           $arrIpTemp[] = $objUser->lastip;
+           $name = $objUser->name;
+           if(empty($name)){
+               $objUser->name = $objUser->phone;
+               $starname = Base_Util_String::starPhone($objUser->phone);
+           }else{
+               $objUser->name = $name;
+               $starname = Base_Util_String::starUsername($name);
+           }
+           fwrite($fpr, $val['id'].",".$val['name'].",".$val['starname'].",".$data.",".$objUser->name.",".$starname.",".strftime('%Y-%m-%d %H:%M:%S',$invite->createTime).",".$objUser->lastip.",".$objUser->huifuid."\n");
+        }
+    }
+    fwrite($fp2,$val['id'].",".$val['name'].",".$val['starname'].",".$val['fake'].",".$val['real'].",".$val['huifu']."\n");
+}
+fclose($fp);
+fclose($fpr);
+fclose($fp1);
+fclose($fp2);
