7a0a5d869892e0ac08d79293408488a7b06ad942
jiangbianliming <jiangbianliming@126.com>
2015-05-29 11:57:16 +0800

    code fix

--- application/modules/Angel/controllers/Api.php
+++ application/modules/Angel/controllers/Api.php
@@ -16,8 +16,8 @@ class ApiController extends Base_Controller_Api{
 	    $logic     = new Awards_Logic_Invite();
 	    $intUserid = $logic->decode($angelCode);
 	    $objUser = User_Api::getUserObject($intUserid);
-	    $name = $objUser->name;
-	    if(empty($name)){
+	    $type = $objUser->usertype;
+	    if($type != User_Type_Roles::TYPE_ANGEL){
 	        return $this->ajaxError(Angel_RetCode::ANGEL_CODE_WRONG,Angel_RetCode::getMsg(Angel_RetCode::ANGEL_CODE_WRONG));
 	    }
 	    $objAngel = new Angel_Object_Angel();
@@ -57,5 +57,24 @@ class ApiController extends Base_Controller_Api{
     	    }
 	    }
 	    $this->ajax($arrRet);
+	}
+	
+	/**
+	 * 根据用户名返回对应的包含天使码的投资详情页链接
+	 */
+	public function getUrlAction(){
+	    $name = isset($_REQUEST['name'])?$_REQUEST['name']:'';
+	    $objUser = new User_Object_Login();
+	    $objUser->fetch(array('name'=>$name));
+	    if($objUser->usertype !== User_Type_Roles::TYPE_ANGEL){
+	        return $this->ajaxError();
+	    }	    
+	    $loan = new Loan_Object_Loan();
+	    $loan->fetch(array('status'=>Loan_Type_LoanStatus::LENDING));
+	    $url   = $this->webroot."/invest/angeldetail?id=$loan->id";
+	    $logic = new Awards_Logic_Invite();
+	    $code  = $logic->encode($objUser->userid);
+	    $url  .= "&angel=".$code;
+	    return $this->ajax($url);
 	}
 }
--- application/modules/Invest/controllers/Success.php
+++ application/modules/Invest/controllers/Success.php
@@ -10,5 +10,19 @@ class SuccessController extends Base_Controller_Response {
 	    $name = isset($_REQUEST['name'])?$_REQUEST['name']:'';
 	    $this->_view->assign('amount', Base_Util_Number::tausendStyle($amount));
 	    $this->_view->assign('name',$name);
-	}
+	    
+	    $objAngel = new Angel_Object_Angel();
+	    $objUser  = new User_Object_Login();
+	    $logic     = new Awards_Logic_Invite();
+	    
+        $objUser->fetch(array('name'=>$name));
+        $user = User_Api::getUserObject($objUser->userid);
+	    
+	    $objAngel->userid     = $this->userid;
+	    $objAngel->angelid    = $user->userid;
+	    $objAngel->angelname  = $user->name;
+	    $objAngel->angelcode  = $logic->encode($user->userid);
+	    $objAngel->angelimage = $user->headurl;
+	    $objAngel->save();
+	} 
 }
