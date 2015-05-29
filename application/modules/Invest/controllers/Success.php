cda04aacb43f6b713c27e66a3e3fccc28bcee2f2
jiangbianliming <jiangbianliming@126.com>
2015-05-29 11:59:29 +0800

    code fix

--- application/modules/Invest/controllers/Success.php
+++ application/modules/Invest/controllers/Success.php
@@ -11,18 +11,20 @@ class SuccessController extends Base_Controller_Response {
 	    $this->_view->assign('amount', Base_Util_Number::tausendStyle($amount));
 	    $this->_view->assign('name',$name);
 	    
-	    $objAngel = new Angel_Object_Angel();
-	    $objUser  = new User_Object_Login();
-	    $logic     = new Awards_Logic_Invite();
-	    
-        $objUser->fetch(array('name'=>$name));
-        $user = User_Api::getUserObject($objUser->userid);
-	    
-	    $objAngel->userid     = $this->userid;
-	    $objAngel->angelid    = $user->userid;
-	    $objAngel->angelname  = $user->name;
-	    $objAngel->angelcode  = $logic->encode($user->userid);
-	    $objAngel->angelimage = $user->headurl;
-	    $objAngel->save();
+	    if(!empty($name)){
+    	    $objAngel = new Angel_Object_Angel();
+    	    $objUser  = new User_Object_Login();
+    	    $logic     = new Awards_Logic_Invite();
+    	    
+            $objUser->fetch(array('name'=>$name));
+            $user = User_Api::getUserObject($objUser->userid);
+    	    
+    	    $objAngel->userid     = $this->userid;
+    	    $objAngel->angelid    = $user->userid;
+    	    $objAngel->angelname  = $user->name;
+    	    $objAngel->angelcode  = $logic->encode($user->userid);
+    	    $objAngel->angelimage = $user->headurl;
+    	    $objAngel->save();
+	    }
 	} 
 }
