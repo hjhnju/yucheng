2400f141670907cc723ee36c976bdf7ff2caa8c0
guojinli <guojinli@xingjiaodai.com>
2015-06-11 16:21:16 +0800

    refs add phone verify limit function

--- application/library/Base/Sms/Weimi.php
+++ application/library/Base/Sms/Weimi.php
@@ -40,6 +40,17 @@ class Base_Sms_Weimi implements Base_Sms_Interface{
             ));
             return false;
         }
+        //检查当天是否还可以继续对该手机号发送验证码
+        if(!$this->checkRecord($mixPhone)) {
+            Base_Log::error(array(
+                'msg'    => '该号码当天不能在继续发送短信',
+                'phone'  => $mixPhone, 
+                'tplid'  => $strTplid,
+                'args'   => $arrArgs,
+                'timing' => $timing,
+            ));
+            return false;
+        }
         $arrPost = array();
         $arrPost['uid'] = $this->uid;
         $arrPost['pas'] = $this->pas;
@@ -81,6 +92,8 @@ class Base_Sms_Weimi implements Base_Sms_Interface{
             'args'   => $arrArgs,
             'timing' => $timing,
         ));
+        //发送完短信后，要记录住该手机号的发送状态信息,不管成功或者失败
+        $this->sendRecord($mixPhone);
         return false;
     }
 
@@ -117,6 +130,50 @@ class Base_Sms_Weimi implements Base_Sms_Interface{
             }
         }
         Base_Log::warn(array('ret'=>$ret, 'phone'=>$mixPhone, 'content'=>$strCtx, 'timing'=>$timing));
+        return false;
+    }
+
+    /**
+     * 记录该手机的请求次数，不管成功发送还是失败发送都会记录
+     * @param  string $phone 手机号码
+     * @return [type] null
+     */
+    public function sendRecord($phone=''){
+        $redis  = Base_Redis::getInstance();
+        $key    = Base_Keys::getPhoneLimitKey($phone); 
+        //每次记录都要给总数加1
+        $redis->hincrby($key, 'count', 1);
+        //将操作时间设置为当前时间
+        $redis->hset($key, 'time', time());
+
+        //判断是否要写入过期时间，如果有过期时间，就不要在重复写入
+        $check = intval($redis->ttl($key));
+        if($check < 0) {
+            //获取时间间隔
+            $interval = mktime(23, 59, 59, date('m', time()), date('d', time()), date('Y', time())) - time();
+            //第一次写入的时候需要制定过期时间
+            $redis->expire($key, $interval);
+        }  
+    }
+
+    /**
+     * 检查该手机号码是否还有继续请求发送的权利
+     * @param  string $phone 手机号码
+     * @return [type] true | false
+     */
+    public function checkRecord($phone=''){
+        $redis  = Base_Redis::getInstance();
+        $key    = Base_Keys::getPhoneLimitKey($phone); 
+        //获取限制条件
+        $config = Base_Config::getConfig('sms', CONF_PATH . '/sms.ini');
+
+        //获取当天的总数
+        $count  = intval($redis->hget($key, 'count'));
+        $time   = intval($redis->hget($key, 'time'));
+        //检查是否可以继续写入,如果总数 - 已发数大于0，并且当前时间-上次发送的最后时间大于时间间隔，那么可以写入
+        if(($config['limit']['count']-$count) > 0 && (time()-$time) > $config['limit']['time'] ) {
+            return true;
+        }
         return false;
     }
 }
